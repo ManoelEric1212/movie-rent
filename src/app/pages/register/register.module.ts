412c5cdc9274f06f3f27428c06a0d06a304a19c3
Matheus Alves <mtalves27@gmail.com>
2023-09-02 00:02:57 -0300

    chore: adjust login and register page

--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './register.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 
+import { RegisterComponent } from './register.component';
+
 @NgModule({
   declarations: [RegisterComponent],
   imports: [
-    CommonModule,
     CommonModule,
     FormsModule,
     MatFormFieldModule,
